338a60b0e5ee030a0886f329c296cc53f792c5f0
MrAfgan <mr.vendetta1987@gmail.com>
2025-12-09 18:24:11 +0400

    Merge pull request #6
    * 25ci derse aid 3dene taski hell eledim
    
    * Merge remote-tracking branch 'origin/Devepol' into Devepol

---
+++ src/lesson25/task1/Task1.java
@@ -0,0 +1,16 @@
+package lesson25.task1;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Task1 {
+    static Predicate<Integer> isEven = n -> n % 2 == 0;
+
+    public static void filterNumbers(List<Integer> list, Predicate<Integer> predicate) {
+        for (Integer n:list){
+            if (predicate.test(n)){
+                System.out.println(n);
+            }
+        }
+    }
+}
---
+++ src/lesson25/task3/Task3.java
@@ -0,0 +1,15 @@
+package lesson25.task3;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Task3 {
+    static Predicate<Integer> isAdult = age -> age >=18;
+    public static void filterPersons(List<Person>list, Predicate<Integer>predicate){
+        for (Person p:list){
+            if (predicate.test(p.getAge())){
+                System.out.println("This person is adult: "+p.getName());
+            }else System.out.println("This person under 18 : "+p.getName());
+        }
+    }
+}
