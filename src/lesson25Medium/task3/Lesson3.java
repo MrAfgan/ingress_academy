a0b61648f4ed03524cce160beb4b18a175608d34
MrAfgan <mr.vendetta1987@gmail.com>
2025-12-16 14:11:33 +0400

    Merge remote-tracking branch 'origin/Devepol' into Devepol
    # Conflicts:
    #	src/lesson26/task7/Task7.java

--- src/lesson25Medium/task3/Lesson3.java
+++ src/lesson25Medium/task3/Lesson3.java
@@ -3,10 +3,9 @@ package lesson25Medium.task3;
 import java.util.List;
 
 public class Lesson3 {
-    static Transformer upperCasePrint = s -> s.toUpperCase();
+    static Transformer upperCasePrint = s-> s.toUpperCase();
     static Transformer reversPrint = s -> String.valueOf(new StringBuilder(s).reverse());
-    static Transformer addSuffix = s -> (s + "_in Java");
-
+    static Transformer addSuffix = s-> s+("_in Java");
     public static void transform(List<String> list, Transformer transform) {
         for (String s : list) {
             System.out.println(transform.transformation(s));
