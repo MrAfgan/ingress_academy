8ee12fa3b2c3cdcbe377f0f09cd42946febeae4e
MrAfgan <mr.vendetta1987@gmail.com>
2025-12-17 00:13:46 +0400

    lesson 26 medium 7th task done

--- src/lesson26Medium/lesson7/Task7.java
+++ src/lesson26Medium/lesson7/Task7.java
@@ -1,7 +1,21 @@
 package lesson26Medium.lesson7;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Task7 {
     public static void main(String[] args) {
-
+        List<String> sentences = List.of(
+                "Hello Java",
+                "its fourth month",
+                "since im learning you",
+                "Last two month of java learning lessons"
+        );
+        System.out.println(
+                sentences.stream()
+                        .flatMap(s -> Arrays.stream(s.split(" ")))
+                        .map(String::toLowerCase)
+                        .collect(Collectors.toSet()));
     }
 }
