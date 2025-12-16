dd379020524c570e8a16ce6fe760e017d2c96465
MrAfgan <mr.vendetta1987@gmail.com>
2025-12-16 17:01:43 +0400

    Merge remote-tracking branch 'origin/Devepol' into Devepol
    # Conflicts:
    #	src/lesson26/task7/Task7.java

--- src/lesson26Medium/lesson4/Task4.java
+++ src/lesson26Medium/lesson4/Task4.java
@@ -1,7 +1,14 @@
 package lesson26Medium.lesson4;
 
+import java.util.List;
+
 public class Task4 {
     public static void main(String[] args) {
-
+        List<Integer> numbs = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        numbs.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(n -> n)
+                .average()
+                .ifPresent(System.out::println);
     }
 }
