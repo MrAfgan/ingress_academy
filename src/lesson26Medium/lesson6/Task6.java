c14b22308199122ec70482540436333bf728fac3
MrAfgan <mr.vendetta1987@gmail.com>
2025-12-16 23:15:47 +0400

    lesson 26 medium 6th task done

--- src/lesson26Medium/lesson6/Task6.java
+++ src/lesson26Medium/lesson6/Task6.java
@@ -1,10 +1,15 @@
 package lesson26Medium.lesson6;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Task6 {
     public static void main(String[] args) {
         List<String> names = List.of("Abbas", "Sabah", "Abseron", "Bomba", "Qarabag");
-        System.out.println(names.stream().filter(s -> s.toUpperCase().contains("A")).count());
+        System.out.println(
+                names.stream()
+                        .flatMap(n -> Arrays.stream(n.split("")))
+                        .map(String::toLowerCase)
+                        .filter(s -> s.contains("a")).count());
     }
 }
