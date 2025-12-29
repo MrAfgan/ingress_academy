86ac8b7d6ad5903b51a0359c33bdfb191fe55595
MrAfgan <mr.vendetta1987@gmail.com>
2025-12-29 20:46:25 +0400

    lesson 26 medium 7th task done

---
+++ src/lesson31/CustomException.java
@@ -0,0 +1,7 @@
+package lesson31;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
--- src/lesson31/Main.java
+++ src/lesson31/Main.java
@@ -1,4 +1,10 @@
 package lesson31;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+    Map<User, List<Order>> order=new HashMap<>();
+
 }
